2f57193734cd1cde35a4c625d33dda0a65e6a892
lukaszdzaman88-rgb <lukaszdzaman88@gmail.com>
2026-02-05 21:24:39 +0100

    11

--- src/main/java/hehex/forsaken/ForsakenRealityDataGenerator.java
+++ src/main/java/hehex/forsaken/ForsakenRealityDataGenerator.java
@@ -1,11 +1,19 @@
 package hehex.forsaken;
 
+import hehex.forsaken.datagen.ModModelProvider;
+import hehex.forsaken.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class ForsakenRealityDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
+
 
 	}
+
 }
